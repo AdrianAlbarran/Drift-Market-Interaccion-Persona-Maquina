bb14ed98f9dc1b770e790022bccee19df5f267be
Moromon <lmoralvxd@gmail.com>
2022-11-14 15:34:10 +0100

    Code
    Juego

---
+++ GDD & PROTOTIPADO/Code/Assets/Scripts/RecogerObjeto.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecogerObjeto : MonoBehaviour
+{
+    public GameObject UI;
+    private GameObject luz;
+    private bool objetoRecogido;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            UI.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == ("Player"))
+        {
+            UI.gameObject.SetActive(false);
+        }
+    }
+    public void Update()
+    {
+
+        if (UI.activeSelf && !objetoRecogido)
+        {
+            if (Input.GetKeyDown("f"))
+            {
+                luz = GameObject.Find("Spot Light");
+                luz.SetActive(false);
+                UI.gameObject.SetActive(false);
+                Destroy(this);
+            }
+        }
+    }
+}
