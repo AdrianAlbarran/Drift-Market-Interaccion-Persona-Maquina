d74ec7c4a41957c5f00ab90a8101b65422f8c761
Adrian <adrianserranomonteiro2@gmail.com>
2022-12-17 14:40:38 +0100

    fix buttons, controller update & new drifting system

--- GDD & PROTOTIPADO/Code/Assets/Scripts/Car.cs
+++ GDD & PROTOTIPADO/Code/Assets/Scripts/Car.cs
@@ -16,10 +16,10 @@ public class Car : MonoBehaviour
     public Transform wheelLeftBack;
     public Transform wheelRightBack;    
 
-    public float motorTorque = 200f;
-    public float maxSteer = 45f;
+    public float motorTorque = 400f;
+    public float maxSteer = 40f;
     public bool isbrake = false;
-    public float brakeTorque = 5000f;
+    public float brakeTorque = 100f;
 
     private Rigidbody _rigidbody;
 
@@ -31,14 +31,27 @@ public class Car : MonoBehaviour
 
     void FixedUpdate()
     {
-        wheelColliderLeftBack.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
-        wheelColliderRightBack.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
-        wheelColliderLeftFront.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
-        wheelColliderRightFront.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
+        if (SimpleInput.GetAxis("Vertical") > 0 | SimpleInput.GetAxis("Vertical") < 0) {
+            wheelColliderLeftBack.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
+            wheelColliderRightBack.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque;
+            wheelColliderLeftFront.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque * 0.2f;
+            wheelColliderRightFront.motorTorque = SimpleInput.GetAxis("Vertical") * motorTorque * 0.2f;
+        } else if (SimpleInput.GetAxis("Acelerate") > 0 | SimpleInput.GetAxis("Acelerate") < 0){
+            wheelColliderLeftBack.motorTorque = SimpleInput.GetAxis("Acelerate") * motorTorque;
+            wheelColliderRightBack.motorTorque = SimpleInput.GetAxis("Acelerate") * motorTorque;
+            wheelColliderLeftFront.motorTorque = SimpleInput.GetAxis("Acelerate") * motorTorque * 0.2f;
+            wheelColliderRightFront.motorTorque = SimpleInput.GetAxis("Acelerate") * motorTorque * 0.2f;
+        } else if (SimpleInput.GetAxis("Back") > 0 | SimpleInput.GetAxis("Back") < 0) {
+            wheelColliderLeftBack.motorTorque = SimpleInput.GetAxis("Back") * -motorTorque;
+            wheelColliderRightBack.motorTorque = SimpleInput.GetAxis("Back") * -motorTorque;
+            wheelColliderLeftFront.motorTorque = SimpleInput.GetAxis("Back") * -motorTorque * 0.2f;
+            wheelColliderRightFront.motorTorque = SimpleInput.GetAxis("Back") * -motorTorque * 0.2f;
+        }
+
         wheelColliderLeftFront.steerAngle = SimpleInput.GetAxis("Horizontal") * maxSteer;
         wheelColliderRightFront.steerAngle = SimpleInput.GetAxis("Horizontal") * maxSteer;
 
-        if (SimpleInput.GetAxis("Brake") == 1) {
+        if (SimpleInput.GetAxis("Brake") > 0) {
             isbrake = true;
         } else {
             isbrake = false;
@@ -47,9 +60,17 @@ public class Car : MonoBehaviour
         if (isbrake == true) {
             wheelColliderLeftBack.brakeTorque = brakeTorque;
             wheelColliderRightBack.brakeTorque = brakeTorque;
+            maxSteer = 65f;
+            maxSteer = 65f;
+            wheelColliderLeftBack.forceAppPointDistance = 0.7f;
+            wheelColliderRightBack.forceAppPointDistance = 0.7f;
         } else {
             wheelColliderLeftBack.brakeTorque = 0;
             wheelColliderRightBack.brakeTorque = 0;
+            maxSteer = 40f;
+            maxSteer = 40f;
+            wheelColliderLeftBack.forceAppPointDistance = 0;
+            wheelColliderRightBack.forceAppPointDistance = 0;
         }
     }
 }
--- GDD & PROTOTIPADO/Code/Assets/Scripts/RecogerObjeto.cs
+++ GDD & PROTOTIPADO/Code/Assets/Scripts/RecogerObjeto.cs
@@ -42,7 +42,7 @@ public class RecogerObjeto : MonoBehaviour
     {
         if (UI.activeSelf && recogido)
         {
-            if (SimpleInput.GetAxis("PickUp") == 1)
+            if (SimpleInput.GetAxis("PickUp") > 0)
             {
                 recogido = false;
                 AudioManager.instance.Play("CogerObjeto");
